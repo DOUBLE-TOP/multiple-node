d987601329ef18e1709a1fe5ad6561740afc9adf
ivan.pohosov <pogosovivan@gmail.com>
2025-01-11 11:55:03 +0200

    Update stats

--- stats.sh
+++ stats.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 DIR="./data/accounts"
-echo -e "Public_key\tRunning_Time"
+
+# Print the header with nice formatting
+echo -e "Public_key\t\t\t\tRunning_Time"
+echo -e "-----------------------------------------------------"
+
+# Collect data to create a structured format for column
+output=""
 
 for file in "$DIR"/*; do
     if [[ -f $file ]]; then
         email=$(awk -F= '/^Public_key=/{print $2}' "$file")
         earnings=$(awk -F= '/^Running_Time=/{print $2}' "$file")
         if [[ -n $email && -n $earnings ]]; then
-            echo -e "$email\t$earnings"
+            output+="$email\t$earnings\n"
         fi
     fi
 done
+
+# Use column to format the output and sort by Total_Earnings
+echo -e "$output" | sort -k2,2n | column -t -s $'\t'
