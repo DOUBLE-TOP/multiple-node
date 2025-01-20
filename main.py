df764078490d9ee6c082766611af050a31cc6360
ivan.pohosov <pogosovivan@gmail.com>
2025-01-20 10:15:49 +0200

    Update stats

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import asyncio
 from src.runner import Runner
 
 if __name__ == "__main__":
-    runner = Runner()
-    asyncio.run(runner.run_accounts())
+    try:
+        runner = Runner()
+        asyncio.run(runner.run_accounts())
+    except KeyboardInterrupt:
+        print("\n👋 Shutting down gracefully...")
